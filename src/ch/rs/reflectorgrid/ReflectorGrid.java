1e0521014d704f2ffbc6bd87751d23dab20e150c
Ricardo <30594398+FancyJavaStuff@users.noreply.github.com>
2017-10-31 21:55:26 +0100

    Update ReflectorGrid.java

--- src/ch/rs/reflectorgrid/ReflectorGrid.java
+++ src/ch/rs/reflectorgrid/ReflectorGrid.java
@@ -1,7 +1,7 @@
 /**
 MIT License
 
-Copyright (c) 2017 Ricardo
+Copyright (c) 2017 Ricardo Daniel Monteiro Simoes
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
